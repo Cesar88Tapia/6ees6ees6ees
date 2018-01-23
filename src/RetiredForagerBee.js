7e95a22778bba0cf229c61a7139e21e928f9024d
Cesar88Tapia <tapia.cesarfelipe88@gmail.com>
2018-01-22 19:26:39 -0600

    finished retired forager bee

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,16 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee{
+  constructor(){
+    super()
+    this.age = 40
+    this.canFly = false
+    this.color = 'grey'
+    this.job = 'gamble'
+  }
+  forage(){
+    return 'I am too old, let me play cards instead'
+  }
+  gamble(treasure){
+    this.treasureChest.push(treasure)
+    return this.treasureChest
+  }
 };
